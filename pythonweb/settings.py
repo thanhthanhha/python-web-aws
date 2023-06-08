308651c85cabd4d8a2d683943c64055c8a12ac84
Thanh Ha <nguyenthanhha.tai.555@gmail.com>
2023-06-08 14:56:26 +0700

    gg

--- pythonweb/settings.py
+++ pythonweb/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-vl%-vph7dtwqmh$^&&1()2zoaur%i@!%okdw^#4z--qkl@g((^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
