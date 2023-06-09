3a1deabd9c510f1525cc07644ddba62e9feb9e9a
Thanh Ha <nguyenthanhha.tai.555@gmail.com>
2023-06-09 09:33:32 +0700

    gg

--- python-web-aws/pythonweb/settings.py
+++ python-web-aws/pythonweb/settings.py
@@ -101,7 +101,7 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+            'NAME': os.path.join(BASE_DIR, os.getenv('DB_FILE'), 'db.sqlite3'),
         }
     }
 
